17c68fb022b28781cbeda11b23f78fb3c62bd000
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-26 01:03:16 -0700

    Betty Update

--- main.h
+++ main.h
@@ -9,16 +9,15 @@ int pstring(va_list s);
 int pdec(va_list d);
 int pchar(va_list c);
 /**
- * struct code_format - struct format
- * 
+ * struct switcher - Pair specifiers to their sutable handler functions
  * @sc: The specifiers
- * @f: The functions associated
+ * @f: The functions handler
  */
 
 typedef struct switcher
 {
 	char *sc;
-	int (*f) (va_list);
+	int (*f)(va_list);
 } _switch;
 
 #endif
