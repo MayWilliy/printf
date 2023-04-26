2be4b07f2fcac6b5d585e2efb262cfdba761e5fe
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-26 07:47:35 -0700

    General update

--- main.h
+++ main.h
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-#ifndef MAIN_H
-#define MAIN_H
-#endif
-
-=======
->>>>>>> 17c68fb022b28781cbeda11b23f78fb3c62bd000
 #ifndef functions
 #define functions
 #include <stdarg.h>
@@ -15,35 +8,17 @@ int pint(va_list i);
 int pstring(va_list s);
 int pdec(va_list d);
 int pchar(va_list c);
-<<<<<<< HEAD
-
-#include <stdlib.h>
-#include <unistd.h>
 /**
- * _putchar - write the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1
- * On error, is returned and error is set appropriately
- */
-
-int _putch(char ch)
-{
-	return (write(1, &ch < 1));
-}
-
-=======
->>>>>>> 17c68fb022b28781cbeda11b23f78fb3c62bd000
-/**
- * struct switcher - Pair specifiers to their sutable handler functions
+ * struct code_format - struct format
+ * 
  * @sc: The specifiers
- * @f: The functions handler
+ * @f: The functions associated
  */
 
 typedef struct switcher
 {
 	char *sc;
-	int (*f)(va_list);
+	int (*f) (va_list);
 } _switch;
 
 #endif
--- specifier_functions.c
+++ specifier_functions.c
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
-#ifndef SPECIFIER_FUNCTIONS_C
-#define SPECIFIER_FUNCTIONS_C
-#include <stdarg.h>
-
-/**
- * pchar- function that print a character
- * @c: character
- * Return: the number of characters printed (excluding the null byte)
- */
-int pchar(va_list, c)
-=======
 #include "main.h"
-/**
- * pchar - Prints a character
- * @c: Character to be printed
- * Return: 1 (ONE)
- */
 int pchar(va_list c)
->>>>>>> 17c68fb022b28781cbeda11b23f78fb3c62bd000
 {
 	unsigned char my_char;
 
@@ -25,33 +7,15 @@ int pchar(va_list c)
 	_putch(my_char);
 	return (1);
 }
-<<<<<<< HEAD
 
-/**
- * pstring- function that print a string
- * @st : string
- * Return: the number of characters printed (excluding the null byte)
- */
-
-int pstring(va_list, str)
-}
-=======
-/**
- * pstring - Prints a string
- * @st: String to be printed
- * Return: Length of the string
- */
 int pstring(va_list st)
 {
->>>>>>> 17c68fb022b28781cbeda11b23f78fb3c62bd000
 	char *str;
 	int idx = 0;
 
-	char *str = va_arg(str, char *);
-
+	str = va_arg(st, char *);
 	if (str == NULL)
 		str = "(null)";
-
 	while (str[idx])
 	{
 		_putch(str[idx]);
@@ -59,24 +23,10 @@ int pstring(va_list st)
 	}
 	return (idx);
 }
-<<<<<<< HEAD
 
-/**
- * pdec - function that print decimal
- * @d : decimal
- * Return: the number of characters printed (excluding the null byte)
- */
-int pdec(va_list, d)
-=======
-/**
- * pdec - Prints a decimal number
- * @d: Decimal to be printed
- * Return: Lenth og the decimal
- */
 int pdec(va_list d)
->>>>>>> 17c68fb022b28781cbeda11b23f78fb3c62bd000
 {
-	int len = 0, pow, j, digit, n, count = 0, num;
+	int len = 0, pow, j ,digit, n, count = 0, num;
 
 	n = va_arg(d, int);
 	if (n != 0)
@@ -107,26 +57,14 @@ int pdec(va_list d)
 			pow /= 10;
 		}
 	}
-	else
+	else 
 	{
-		_putch('0');
+		_putch ('0');
 		return (1);
 	}
 	return (count);
 }
-<<<<<<< HEAD
 
-/**
- * pint- fucntion that print an integer
- * @i : integer
- * Return: the number of characters printed (excluding the null byte)
-=======
-/**
- * pint - Prints and integer
- * @i: Integer to be printed
- * Return: Length of the integer
->>>>>>> 17c68fb022b28781cbeda11b23f78fb3c62bd000
- */
 int pint(va_list i)
 {
 	unsigned int len, power, j;
@@ -137,42 +75,24 @@ int pint(va_list i)
 	{
 		if (n < 0)
 		{
-			_putch('-');
+			_putch ('-');
 			count++;
 		}
-
 		num = n;
 		len = 0;
-
 		while (num != 0)
 		{
 			num /= 10;
 			len++;
 		}
-
 		power = 1;
 		for (j = 1; j <= len - 1; j++)
 			power *= 10;
-<<<<<<< HEAD
-
-			for (j = 1; j <= len; j++)
-			{
-				digit = n / power;
-				if (n < 0)
-					_putch((digit * -1) + 48);
-				else
-					_putch(digit + '0');
-					count++;
-
-				n -= digit * power;
-				power /= 10;
-			}
-=======
 		for (j = 1; j <= len; j++)
 		{
 			digit = n / power;
 			if (n < 0)
-				_putch((digit * -1) + 48);
+				_putch((digit * - 1) + 48);
 			else
 				_putch(digit + '0');
 			count++;
@@ -180,17 +100,11 @@ int pint(va_list i)
 			n -= digit * power;
 			power /= 10;
 		}
->>>>>>> 17c68fb022b28781cbeda11b23f78fb3c62bd000
 	}
 	else
 	{
-		_putch('0');
+		_putch ('0');
 		return (1);
 	}
 	return (count);
 }
-<<<<<<< HEAD
-
-#endif
-=======
->>>>>>> 17c68fb022b28781cbeda11b23f78fb3c62bd000
